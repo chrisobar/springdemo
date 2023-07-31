1876ff48eac42ea17ee6c7ab5d597e21647b33d5
HOME <HOME@LL-CCOBAR.local>
2023-07-31 16:25:36 +0800

    TEST

---
+++ swing_demo/src/main/java/com/example/swing_demo/SwingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.swing_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwingDemoApplication.class, args);
+	}
+
+}
---
+++ swing_demo/src/test/java/com/example/swing_demo/SwingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swing_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
